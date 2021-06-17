b039099c7d3674a4331d3f0e0f514a789ac37b3c
yasminwaldeck <yasmin.waldeck@posteo.de>
2021-06-17 11:11:59 +0200

    Implement stuff coaches pointed out

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import SearchPage from "./pages/SearchPage";
 import Header from "./components/Header";
+import TypeProvider from "./context/TypeProvider";
 
 function App() {
   return (
     <div className="App">
+        <TypeProvider>
         <Header/>
         <SearchPage/>
+    </TypeProvider>
     </div>
   );
 }
--- frontend/src/pages/SearchPage.js
+++ frontend/src/pages/SearchPage.js
@@ -1,47 +1,45 @@
-import {useRef, useState} from "react";
+import {useContext, useRef, useState} from "react";
 import axios from "axios";
 import MovieCard from "../components/MovieCard";
+import TypeContext from "../context/TypeContext";
 
 export default function SearchPage(){
 
+    const {MOVIE, SERIES} = useContext(TypeContext)
     const [searchString, setSearchString] = useState("")
-    const [searchType, setSearchType] = useState("movie")
+    const [searchType, setSearchType] = useState(MOVIE)
     const [searchResults, setSearchResults] = useState([])
     const timerId = useRef(0)
 
+
     const clickHandler = (searchString, searchType) => {
-        if (searchString !== ""){
-            if(searchType === "movie") {
+        if(searchType === MOVIE) {
                 timerId.current = setTimeout(() => {
                     axios
                         .get(`/api/movie?searchString=${searchString}`)
                         .then(response => response.data)
                         .then((data) => (
-                            setSearchResults(data),
-                            console.log(data)
+                            setSearchResults(data)
                         ))
                         .catch(error => console.log(error))
                 }, 600)
-            }
         }
     }
 
     return(
-        <>
         <div>
             <input type="text"
                    value={searchString}
                    onChange={(event) => setSearchString(event.target.value)}
             />
             <div>
-                <input type="radio" name="movie" onChange={() => setSearchType("movie")} defaultChecked/> Movie
-                <input type="radio" name="series" onChange={() => setSearchType("series")}/> Series
+                <input type="radio" name="movie" onChange={() => setSearchType(MOVIE)} defaultChecked/> Movie
+                <input type="radio" name="series" onChange={() => setSearchType(SERIES)}/> Series
             </div>
             <button disabled={searchString === ""} onClick={() => clickHandler(searchString, searchType)}>Search</button>
             {searchResults.map((movie) => (
                 <MovieCard key={movie.imdbID} movie={movie}/>
             ))}
         </div>
-        </>
     )
 }

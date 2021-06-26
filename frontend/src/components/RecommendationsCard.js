24d0611b0c78c7ca97bb71d5092a8cea6bd40c9f
yasminwaldeck <82826790+yasminwaldeck@users.noreply.github.com>
2021-06-26 10:24:32 +0200

    Add Stats to frontend

---
+++ frontend/src/components/RecommendationsCard.js
@@ -0,0 +1,19 @@
+import MovieAndSeriesCard from "./MovieAndSeriesCard";
+import useWatchlist from "../hooks/useWatchlist";
+import useWatchHistory from "../hooks/useWatchHistory";
+import useDetails from "../hooks/useDetails";
+
+export default function RecommendationsCard({imdbID}){
+    const { watchlist } = useWatchlist();
+    const { watchHistory } = useWatchHistory();
+    const { item } = useDetails(imdbID)
+
+    return(
+        <MovieAndSeriesCard
+            key={imdbID}
+            item={item}
+            onWatchlist={watchlist.find(watchedItem => watchedItem.imdbID === item.imdbID)}
+            onWatchHistory={watchHistory.find(watchedItem => watchedItem.imdbID === item.imdbID)}
+        />
+    )
+}
---
+++ frontend/src/components/StatsCard.js
@@ -0,0 +1,22 @@
+import {useState} from "react";
+import RecommendationsCard from "./RecommendationsCard";
+
+export default function StatsCard({recommendationDetails}){
+
+
+    const [showing, setShowing] = useState();
+
+    return(
+
+        <div>
+            <p>Name: {recommendationDetails.name}</p>
+            <p>Score: {recommendationDetails.number}</p>
+            <button onClick={() => setShowing(!showing)}>Show recommendations</button>
+            <div style={{ display: (showing ? 'block' : 'none') }}>
+                {recommendationDetails.recommendations && recommendationDetails.recommendations.map((recommendation) => (
+                    <RecommendationsCard imdbID={recommendation}/>
+                ))}
+            </div>
+        </div>
+    )
+}
